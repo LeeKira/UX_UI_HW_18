c44a7de3e76c1103722c08bd88acb4f04652a5d8
leestayfancy <leekirasmith14@gmail.com>
2021-11-01 22:01:02 -0400

    Scratched old design. Here's the new design!

---
+++ index.js
@@ -0,0 +1,7 @@
+<!--JQuery-->
+            $(document).ready(function(){
+                $(".toggle").toggleClass("active")
+                $("nav").toggleClass("active")
+            })
+        })
+           
